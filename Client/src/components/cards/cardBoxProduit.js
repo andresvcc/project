7a311e1a1d94f4cad42da8e3edceb92f9f255955
andresvcc <43545168+andresvcc@users.noreply.github.com>
2019-04-30 13:39:54 +0200

    enlever des dependences sans utilisation

--- Client/src/components/cards/cardBoxProduit.js
+++ Client/src/components/cards/cardBoxProduit.js
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 
